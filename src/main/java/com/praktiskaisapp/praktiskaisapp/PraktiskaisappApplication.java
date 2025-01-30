a0c3d0050ee13d53a095d25a40a9c38a50656e88
RoboStriker2k <rlaimite@gmail.com>
2025-01-30 13:10:05 +0200

    first commit

---
+++ src/main/java/com/praktiskaisapp/praktiskaisapp/PraktiskaisappApplication.java
@@ -0,0 +1,13 @@
+package com.praktiskaisapp.praktiskaisapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktiskaisappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktiskaisappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktiskaisapp/praktiskaisapp/PraktiskaisappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktiskaisapp.praktiskaisapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktiskaisappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
